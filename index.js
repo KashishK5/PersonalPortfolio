47298a4f02a11e4a43bcc44cfc7af7e65cdebaf8
KashishK58 <kashishk0508@gmail.com>
2024-06-13 16:43:14 +0530

    new commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function setActiveLink() {
     const navLinks = document.querySelectorAll('.navbar a');
   
@@ -38,45 +37,4 @@ function setActiveLink() {
     setActiveLink();
     checkSection();
   });
-=======
-function setActiveLink() {
-    const navLinks = document.querySelectorAll('.navbar a');
-  
-    navLinks.forEach(link => {
-      link.addEventListener('click', function() {
-        navLinks.forEach(link => link.classList.remove('active'));
-        this.classList.add('active');
-      });
-    });
-  }
-  
-  function checkSection() {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('.navbar a');
-  
-    function makeSectionActive() {
-      const fromTop = window.scrollY;
-  
-      sections.forEach(section => {
-        const sectionTop = section.offsetTop - 50;
-        const sectionHeight = section.clientHeight;
-        const sectionId = section.getAttribute('id');
-        const navLink = document.querySelector(`.navbar a[href="#${sectionId}"]`);
-  
-        if (fromTop >= sectionTop && fromTop <= sectionTop + sectionHeight) {
-          navLinks.forEach(link => link.classList.remove('active'));
-          navLink.classList.add('active');
-        }
-      });
-    }
-  
-    window.addEventListener('scroll', makeSectionActive);
-    window.addEventListener('load', makeSectionActive);
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    setActiveLink();
-    checkSection();
-  });
->>>>>>> origin/port
   
